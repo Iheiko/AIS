19efa9e925f421660b3ba46d894c11fd152cf46f
Iheiko <sammorow@gmail.com>
2017-12-12 10:26:15 +0300

    chrooting

--- install.sh
+++ install.sh
@@ -3,3 +3,16 @@
 timedatectl set-ntp true
 
 printf "g\nn\n\n\n+200M\nt\n1\nn\n\n\n\nw\n" | fdisk /dev/sda
+
+mkfs.vfat /dev/sda1
+mkfs.ext4 /dev/sda2
+
+mount /dev/sda2 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+pacstrap /mnt base base-devel
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
