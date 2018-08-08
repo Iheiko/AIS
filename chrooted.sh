925889079b3f5a61c669883d295de50f9216dcb5
Viktor <sammorow@gmail.com>
2018-08-08 15:44:59 +0300

    Added comments and some style fixes

--- chrooted.sh
+++ chrooted.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
+#
+#Minor install script for chrooted environment
 
+#Time configuration. Set timezone to Moscow and set the Hardware Clock from the System Clock
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 hwclock --systohc
 
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+#Locale configuration. Set default locale to en_US.UTF-8.
+echo "en_US.UTF-8 UTF-8" \
+    >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "LANG=en_US.UTF-8" \
+    >> /etc/locale.conf
 
+#Hostname configuration
+#TODO:
+#     Read hostname from stdin
 echo "ArchVB" >> /etc/hostname 
 echo -e "127.0.0.1	localhost.localdomain	localhost\n\
 ::1		localhost.localdomain	localhost\n\
-127.0.1.1	ArchVB.localdomain	ArchVB" >> /etc/hosts
+127.0.1.1	ArchVB.localdomain	ArchVB" \
+    >> /etc/hosts
 
+#Bootloader installation. Deafault bootloader is systemd-boot
 bootctl --path=/boot install
 
+#Bootloader configuration
 cp /usr/share/systemd/bootctl/* /boot/loader/
 cd /boot/loader/
 mv arch.conf entries
 cd entries
-
 PARTUUID=$(blkid -o value -s PARTUUID /dev/sda2)
 echo "PARTUUID=$PARTUUID"
 sed -i -e 's/PARTUUID=XXXX/PARTUUID='$PARTUUID'/;s/rootfstype=XXXX/rootfstype=ext4/' arch.conf
+
 passwd 
 systemctl enable dhcpcd
 echo DONE!!
--- install.sh
+++ install.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
+#
+# Main install script
 
+#save current dirrectory path
 PWD=$(pwd)
 
 timedatectl set-ntp true
 
-printf "g\nn\n\n\n+200M\nt\n1\nn\n\n\n\nw\n" | fdisk /dev/sda
+#Make new GUID partition table with on /dev/sda
+#New partition table will be like:
+#/dev/sda1 /boot EFI  200M
+#/dev/sda2 /     ext4 rest
+printf "g\nn\n\n\n+200M\nt\n1\nn\n\n\n\nw\n" \
+    | fdisk /dev/sda
+yes y \
+    | mkfs.vfat /dev/sda1
+yes y \
+    | mkfs.ext4 /dev/sda2
 
-yes y|mkfs.vfat /dev/sda1
-yes y|mkfs.ext4 /dev/sda2
-
+#Mount new parttions to /mnt
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
-cat <(grep 'Russia' -A1 /etc/pacman.d/mirrorlist) <(grep -v 'Russia' -A1 /etc/pacman.d/mirrorlist) | sed -e 's/--//g' > .mirrorlist.tmp
+#Generate mirrorlist with Russian repo priority
+cat <(grep 'Russia' -A1 /etc/pacman.d/mirrorlist) \
+    <(grep -v 'Russia' -A1 /etc/pacman.d/mirrorlist) \
+    | sed -e 's/--//g' > .mirrorlist.tmp
 mv .mirrorlist.tmp /etc/pacman.d/mirrorlist
 
 pacstrap /mnt base base-devel
 
+#Save current mount state
 genfstab -U /mnt >> /mnt/etc/fstab
 
+#Run minor install script in chrooted environment
 cp $PWD/chrooted.sh /mnt/root/chrooted.sh
 chmod a+x /mnt/root/chrooted.sh
-
 arch-chroot /mnt bash root/chrooted.sh
 rm /mnt/root/chrooted.sh
 
+#Unmount all parttions from /mnt
 umount -R /mnt
