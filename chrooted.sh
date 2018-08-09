c549eccd525bf5823fcd8d6679578d7c03cc8830
wh75er <bokseng12@gmail.com>
2018-08-09 15:13:23 +0300

    Add host configuration function

--- chrooted.sh
+++ chrooted.sh
@@ -7,6 +7,18 @@ set_time() {
 	ln -sf /usr/share/zoneinfo/${timezone} /etc/localtime
 	hwclock --systohc
 }
+set_locale() {
+	echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+	locale-gen
+	echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+}
+set_hostname() {
+	local hname=${1}
+	echo ${hname} >> /etc/hostname 
+	echo -e "127.0.0.1	localhost.localdomain	localhost\n\
+	::1		localhost.localdomain	localhost\n\
+	127.0.1.1	${hname}.localdomain	${hname}" >> /etc/hosts
+}
 setup_systemd_boot() {
 	local disk=${1}
 	#Bootloader installation. Deafault bootloader is systemd-boot.
@@ -27,16 +39,12 @@ setup_systemd_boot() {
 #Set timezone to Moscow and set the Hardware Clock from the System Clock
 set_time "Europe/Moscow"
 
+
 #Locale configuration. Set default locale to en_US.UTF-8.
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+set_locale
 
 #Hostname configuration
-echo "ArchVB" >> /etc/hostname 
-echo -e "127.0.0.1	localhost.localdomain	localhost\n\
-::1		localhost.localdomain	localhost\n\
-127.0.1.1	ArchVB.localdomain	ArchVB" >> /etc/hosts
+set_hostname ArchVB
 
 #Install and configure systemd-boot
 setup_systemd_boot sda
