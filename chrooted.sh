34083a79daaf4b50af2d6702d643aebb9741eb22
Viktor <sammorow@gmail.com>
2018-08-09 17:29:16 +0300

    removed tabs

--- chrooted.sh
+++ chrooted.sh
@@ -7,32 +7,32 @@ if [ -z "${TIMEZONE}" ]; then
 fi
 
 set_time() {
-	local timezone=${1}
-	ln -sf /usr/share/zoneinfo/${timezone} /etc/localtime
-	hwclock --systohc
+    local timezone=${1}
+    ln -sf /usr/share/zoneinfo/${timezone} /etc/localtime
+    hwclock --systohc
 }
 set_locale() {
-	echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-	locale-gen
-	echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+    echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 }
 set_hostname() {
-	local hname=${1}
-	echo ${hname} >> /etc/hostname 
-	echo -e "127.0.0.1	localhost.localdomain	localhost\n\
-	::1		localhost.localdomain	localhost\n\
-	127.0.1.1	${hname}.localdomain	${hname}" >> /etc/hosts
+    local hname=${1}
+    echo ${hname} >> /etc/hostname 
+    echo -e "127.0.0.1  localhost.localdomain   localhost\n\
+    ::1     localhost.localdomain   localhost\n\
+    127.0.1.1   ${hname}.localdomain    ${hname}" >> /etc/hosts
 }
 setup_systemd_boot() {
-	local disk=${1}
-	bootctl --path=/boot install
-	cp /usr/share/systemd/bootctl/* /boot/loader/
-	cd /boot/loader/
-	mv arch.conf entries
-	cd entries
-	PARTUUID=$(blkid -o value -s PARTUUID /dev/${disk}2)
-	echo "PARTUUID=$PARTUUID"
-	sed -i -e 's/PARTUUID=XXXX/PARTUUID='$PARTUUID'/;s/rootfstype=XXXX/rootfstype=ext4/' arch.conf
+    local disk=${1}
+    bootctl --path=/boot install
+    cp /usr/share/systemd/bootctl/* /boot/loader/
+    cd /boot/loader/
+    mv arch.conf entries
+    cd entries
+    PARTUUID=$(blkid -o value -s PARTUUID /dev/${disk}2)
+    echo "PARTUUID=$PARTUUID"
+    sed -i -e 's/PARTUUID=XXXX/PARTUUID='$PARTUUID'/;s/rootfstype=XXXX/rootfstype=ext4/' arch.conf
 }
 
 #Time configuration
