2eee8b18aee62d49e2b62fc200193de405985953
wh75er <bokseng12@gmail.com>
2018-08-09 14:22:43 +0300

    Two functions initilized
    Was created two functions, which provides time configuration
    and systemd-boot instalation+configuration
    (set_time and setup_systemd_boot)

--- chrooted.sh
+++ chrooted.sh
@@ -2,10 +2,31 @@
 #
 #Minor install script for chrooted environment
 
+set_time() {
+	local region=${1}
+	local city=${2}
+	ln -sf /usr/share/zoneinfo/${region}/${city} /etc/localtime
+	hwclock --systohc
+}
+setup_systemd_boot() {
+	local disk = ${1}
+	#Bootloader installation. Deafault bootloader is systemd-boot.
+	bootctl --path=/boot install
+
+	#Bootloader configuration
+	cp /usr/share/systemd/bootctl/* /boot/loader/
+	cd /boot/loader/
+	mv arch.conf entries
+	cd entries
+	PARTUUID=$(blkid -o value -s PARTUUID /dev/${disk}2)
+	echo "PARTUUID=$PARTUUID"
+	sed -i -e 's/PARTUUID=XXXX/PARTUUID='$PARTUUID\
+	'/;s/rootfstype=XXXX/rootfstype=ext4/' arch.conf
+}
+
 #Time configuration
 #Set timezone to Moscow and set the Hardware Clock from the System Clock
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-hwclock --systohc
+set_time "Europe" "Moscow"
 
 #Locale configuration. Set default locale to en_US.UTF-8.
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
@@ -18,18 +39,8 @@ echo -e "127.0.0.1	localhost.localdomain	localhost\n\
 ::1		localhost.localdomain	localhost\n\
 127.0.1.1	ArchVB.localdomain	ArchVB" >> /etc/hosts
 
-#Bootloader installation. Deafault bootloader is systemd-boot.
-bootctl --path=/boot install
-
-#Bootloader configuration
-cp /usr/share/systemd/bootctl/* /boot/loader/
-cd /boot/loader/
-mv arch.conf entries
-cd entries
-PARTUUID=$(blkid -o value -s PARTUUID /dev/sda2)
-echo "PARTUUID=$PARTUUID"
-sed -i -e 's/PARTUUID=XXXX/PARTUUID='$PARTUUID\
-'/;s/rootfstype=XXXX/rootfstype=ext4/' arch.conf
+#Install and configure systemd-boot
+setup_systemd_boot sda
 
 passwd 
 systemctl enable dhcpcd
